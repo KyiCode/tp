2f6ba65b5cf47ec65ffb14cb58bb90750140d6e5
DPI2-HSS-HP217 <187776668+DPI2-HSS-HP217@users.noreply.github.com>
2026-03-13 10:21:16 +0800

    more bugfixes weeeee

--- src/test/java/seedu/address/logic/parser/AddressBookEditingParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookEditingParserTest.java
@@ -1,16 +1,12 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Application;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
@@ -29,8 +25,4 @@ public class AddressBookEditingParserTest {
                 + PersonUtil.getEditPersonDescriptorDetails(descriptor));
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
-
-    // These two tests below cannot pass at the moment until EditCommand is updated to
-    // use the correct control flow logic
-    
 }
